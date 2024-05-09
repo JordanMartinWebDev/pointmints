9f4640a985222bfda08c783daac83fb607f0e43f
Jordan Martin <jordan.martin2890@gmail.com>
2024-05-09 07:14:52 -0500

    init server, setup cors, setup helpers, init readiness

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
+	"time"
 
 	"github.com/jordanmartinwebdev/pointmints/internal/database"
 
@@ -16,13 +19,48 @@ type apiConfig struct {
 }
 
 func main() {
-	godotenv.Load()
-	dbURL := os.Getenv("DATABASE_URL")
-
-	db, err := sql.Open("libsql", dbURL)
+	err := godotenv.Load(".env")
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to open db %s: %s", dbURL, err)
-		os.Exit(1)
+		log.Printf("warning: assuming default configuration. .env unreadable: %v", err)
 	}
-	defer db.Close()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT environment variable is not set")
+	}
+
+	apiCfg := apiConfig{}
+
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		log.Println("DATABASE_URL environment variable is not set")
+		log.Println("Running without CRUD endpoints")
+	} else {
+		db, err := sql.Open("libsql", dbURL)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "failed to open db %s: %s", dbURL, err)
+			os.Exit(1)
+		}
+		defer db.Close()
+		dbQueries := database.New(db)
+
+		apiCfg.DB = dbQueries
+	}
+
+	mux := http.NewServeMux()
+	corsMux := middlewareCors(mux)
+
+	//Endpoints
+
+	//Users
+	mux.HandleFunc("GET /v1/readiness", apiCfg.handlerReady)
+
+	srv := &http.Server{
+		Addr:              ":" + port,
+		Handler:           corsMux,
+		ReadHeaderTimeout: 5 * time.Second,
+	}
+
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(srv.ListenAndServe())
 }
